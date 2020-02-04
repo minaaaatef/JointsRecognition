088790bd5a7724377c485735a6d8d01d399a8c72
minaaaatef <mina.atef0@gmail.com>
2020-02-04 16:45:52 +0200

    lstm Generator

--- LSTM.py
+++ LSTM.py
@@ -1,31 +1,87 @@
-from keras.models import Sequential
+from keras.models import Sequential,model_from_json
 from keras.layers import LSTM, Dense
+from keras.utils import to_categorical
 import numpy as np
+from Inputprocessing.InputProcessingv2 import InputProccessing
+import os
 
-data_dim = 34
-timesteps = 8
-num_classes = 10
 
-# expected input data shape: (batch_size, timesteps, data_dim)
-model = Sequential()
-model.add(LSTM(32, return_sequences=True,
-               input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32))  # return a single vector of dimension 32
-model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
 
-# Generate dummy training data
-x_train = np.random.random((1000, timesteps, data_dim))
-y_train = np.random.random((1000, num_classes))
 
-# Generate dummy validation data
-x_val = np.random.random((100, timesteps, data_dim))
-y_val = np.random.random((100, num_classes))
+def prepare_input_Matrix():
+    print("training The data")
+    path = r'dataset'
+    y_train = []
+    count = 1
 
-model.fit(x_train, y_train,
-          batch_size=64, epochs=5,
-          validation_data=(x_val, y_val))
+    labels = {'clap':0,
+              'climb':1,
+              'climb_stairs':2,
+              'hit':3,
+              'jump':4,
+              'kick':5,
+              'pick':6,
+              'punch':7,
+              'push':8,
+              'run':9,
+              'sit':10,
+              'situp':11,
+              'stand':12,
+              'turn':13,
+              'walk':14,
+              'wave':15,
+              }
+    epochs = 1
+
+    while True:
+        print("epochs {}".format(epochs))
+        epochs += 1
+        for subdir, dirs, files in os.walk(path,topdown=True):
+            for file in files:
+
+                try:
+                    x_train = InputProccessing(0.9,os.path.join(subdir,file),20)
+                    y_train = labels[os.path.basename(subdir)]
+                except:
+                    pass
+
+
+        y_train = to_categorical(y_train,num_classes=16)
+
+        yield np.array(x_train),y_train
+
+
+def model ():
+
+
+    data_dim = 34
+    num_classes = 16
+
+
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True,
+                input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
+    model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    model.add(LSTM(32))  # return a single vector of dimension 32
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+
+    model.summary()
+
+    model.fit_generator(generator=prepare_input_Matrix(),steps_per_epoch=2593,epochs=1,shuffle=True)
+    print ('training finshed')
+
+    model.evaluate_generator(generator=prepare_input_Matrix())
+    print ('evaluate finshed')
+
+    model.save('model.h5')
+
+    model.save_weights("model_weights.h5")
+
+
+model ()
+
--- tools/extract_points.py
+++ tools/extract_points.py
@@ -1,1 +1,29 @@
 import os
+
+
+path = r"/home/mina_atef0/Desktop/AlphaPose/chossen"
+
+count = 0
+# command = "source venv/bin/activate"
+# os.system(command)
+# os.system("which python3")
+couldRename = 0
+
+for subdir, dirs, files in os.walk(path,topdown=True):
+  for file in files:
+      if str(file).endswith('.json'):
+        continue
+      count += 1    
+      print("proccessing file#" + str(count)) 
+      print(file) 
+      pathtofile =  os.path.join(subdir,file)
+      command = "python3 video_demo.py --video {} --outdir {}".format(pathtofile,subdir)
+      os.system(command)
+      try:
+        os.rename(os.path.join(subdir,'alphapose-results.json'),os.path.join(subdir,file+'.json'))
+      except:
+        couldRename += 1
+
+print("couldn't rename  " +str(couldRename) )
+
+print("made  " +str(count) )
