43e408bd7e8852fad81c24ca122638cde24d8ef6
minaaaatef <mina.atef0@gmail.com>
2020-02-08 20:40:56 +0200

    3 model training

--- LSTM.py
+++ LSTM.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential,model_from_json
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, BatchNormalization,Dropout
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 import numpy as np
@@ -8,8 +8,9 @@ import os
 import random
 from keras.utils import Sequence
 import multiprocessing
-
-
+import matplotlib.pyplot as plt
+import pickle
+#
 # class data_generator(Sequence):
 #
 #     def __init__(self, path):
@@ -61,9 +62,9 @@ import multiprocessing
 #
 #     def __getitem__(self, idx):
 #             return self.all_data[idx-1][0],self.all_data[idx-1][1]
+#
 
-
-def prepare_input_Matrix(path):
+def save_list (path):
     print("training The data")
     y_train = []
     all_data = []
@@ -104,43 +105,120 @@ def prepare_input_Matrix(path):
                 all_data.append(data)
             except:
                 print("try failed")
+
+
+    pickle_out = open("dev.pickle", "wb")
+    pickle.dump(all_data, pickle_out)
+    pickle_out.close()
+
+
+def prepare_input_Matrix(mode="dev"):
+    if mode == "dev":
+        pickle_in = open("data/dev.pickle", "rb")
+        all_data = pickle.load(pickle_in)
+    else:
+        pickle_in = open("data/dataset.pickle", "rb")
+        all_data = pickle.load(pickle_in)
     while True:
         random.shuffle(all_data)
-        print("epochs {}".format(epochs))
-        epochs += 1
         for data in all_data:
             data = list(data)
             yield data[0], data[1]
 
 
-def model ():
-
+def patchnormmodel ():
 
     data_dim = 34
     num_classes = 16
 
     model = Sequential()
-    model.add(LSTM(32, return_sequences=True,
-                input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
+    model.add(LSTM(32, return_sequences=True,input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
+    model.add(BatchNormalization())
     model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    model.add(BatchNormalization())
     model.add(LSTM(32))  # return a single vector of dimension 32
+    model.add(BatchNormalization())
     model.add(Dense(num_classes, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
                 optimizer='adam',
                 metrics=['accuracy'])
 
-    save_model = ModelCheckpoint('weights{epoch:08d}.h5',
+    save_model = ModelCheckpoint('patchnormmodel/weights{epoch:08d}.h5',
                                          save_weights_only=False, period=10)
     model.summary()
-    model.fit_generator(generator=prepare_input_Matrix(r'dataset'),steps_per_epoch=2129,epochs=500,callbacks=[save_model])
+
+
+
+    model.fit_generator(generator=prepare_input_Matrix("dataset"),steps_per_epoch=2129,epochs=500,callbacks=[save_model])
+    # print ('training finshed')
+
+
+
+
+
+
+def biggermodel ():
+
+
+    data_dim = 34
+    num_classes = 16
+
+    model = Sequential()
+    model.add(LSTM(256, return_sequences=True, input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
+    # model.add(BatchNormalization())
+    model.add(LSTM(256, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    model.add(LSTM(256, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    # model.add(BatchNormalization())
+    model.add(LSTM(256))  # return a single vector of dimension 32
+    # model.add(BatchNormalization())
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+
+    save_model = ModelCheckpoint('biggermodel/weights{epoch:08d}.h5',
+                                         save_weights_only=False, period=10)
+    model.summary()
+
+
+
+    model.fit_generator(generator=prepare_input_Matrix("dataset"),steps_per_epoch=2129,epochs=500,callbacks=[save_model])
     print ('training finshed')
 
-    model.evaluate_generator(generator=prepare_input_Matrix(r'validtion set'),steps=514)
-    print ('evaluate finshed')
-
-    model.save('model.h5')
 
 
-model ()
+def dropout ():
 
+
+    data_dim = 34
+    num_classes = 16
+
+    model = Sequential()
+    model.add(LSTM(256, return_sequences=True, input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
+    model.add(Dropout(0.5))
+    model.add(LSTM(256, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    model.add(LSTM(256, return_sequences=True))  # returns a sequence of vectors of dimension 32
+    model.add(Dropout(0.5))
+    model.add(LSTM(256))  # return a single vector of dimension 32
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+
+    save_model = ModelCheckpoint('dropout/weights{epoch:08d}.h5',
+                                         save_weights_only=False, period=10)
+    model.summary()
+
+
+
+    model.fit_generator(generator=prepare_input_Matrix("dataset"),steps_per_epoch=2129,epochs=500,callbacks=[save_model])
+    print ('training finshed')
+
+
+patchnormmodel()
+dropout ()
+biggermodel()
